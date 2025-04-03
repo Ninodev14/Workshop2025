48c38a6a465b5de081bd4b4264ca071a52ad187c
Ninodev14 <nino.lefort@mmibordeaux.com>
2025-04-03 11:43:10 +0200

    add serv and peer

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Servir les fichiers statiques
+app.use(express.static('public'));
+
+// Connexion de Socket.io
+io.on('connection', (socket) => {
+    console.log('Un joueur s\'est connecté');
+
+    // Gérer les événements ici
+    socket.on('joinGame', (gameData) => {
+        console.log(gameData);
+        socket.emit('gameUpdate', gameData);
+    });
+
+    socket.on('addIngredient', (ingredientData) => {
+        io.emit('ingredientAdded', ingredientData);
+    });
+
+    socket.on('nextPlayer', (currentPlayer) => {
+        io.emit('switchPlayer', currentPlayer);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Un joueur a quitté');
+    });
+});
+
+// Lancer le serveur sur le port 3000
+server.listen(3000, () => {
+    console.log('Serveur en écoute sur le port 3000');
+});
