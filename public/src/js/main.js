9354896a99d4657770be8337b1a3401985df6e36
Ninodev14 <nino.lefort@mmibordeaux.com>
2025-04-07 10:35:34 +0200

    add main zone

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,9 +1,21 @@
 const socket = io();
 
 let playerRole = null;
+let maxIngredients = 1;
 const roomId = new URLSearchParams(window.location.search).get('roomId');
 const playerId = localStorage.getItem("playerId");
 const playerName = localStorage.getItem("playerName");
+
+
+const additionalImages = [
+    "src/img/ingredients/tomato.png",
+    "src/img/ingredients/cheese.png",
+    "src/img/ingredients/lettuce.png",
+    "src/img/ingredients/cucumber.png",
+    "src/img/ingredients/pepperoni.png"
+];
+
+let weightedPool = additionalImages.slice();
 
 socket.emit('requestRole', roomId, playerId, (response) => {
     if (response.success) {
@@ -49,13 +61,41 @@ function readyToStartP2() {
     socket.emit("playerReadyForGame", roomId, playerId);
 }
 
-const additionalImages = [
-    "src/img/ingredients/tomato.png",
-    "src/img/ingredients/cheese.png",
-    "src/img/ingredients/lettuce.png",
-    "src/img/ingredients/cucumber.png",
-    "src/img/ingredients/pepperoni.png"
-];
+function clearIngredientZone(zoneId) {
+    const zone = document.getElementById(zoneId);
+    zone.innerHTML = '';
+}
+
+function spawnRandomIngredient(zoneId) {
+    const randomImage = weightedPool[Math.floor(Math.random() * weightedPool.length)];
+    const animationZone = document.getElementById(zoneId);
+
+    const randomImg = document.createElement("img");
+    const altText = randomImage.split('/').pop().replace('.png', '');
+
+    randomImg.src = randomImage;
+    randomImg.alt = altText;
+    randomImg.classList.add("ingredient-img");
+    randomImg.draggable = true;
+    randomImg.style.animationDelay = '0s';
+
+    animationZone.appendChild(randomImg);
+    registerInitialZone(randomImg, animationZone);
+}
+
+function startIngredientSpawning(zoneId) {
+    setInterval(() => {
+        spawnRandomIngredient(zoneId);
+    }, 2000);
+}
+
+function startGame() {
+    clearIngredientZone("Player1IngredientZone");
+    clearIngredientZone("Player2IngredientZone");
+
+    startIngredientSpawning("Player1IngredientZone");
+    startIngredientSpawning("Player2IngredientZone");
+}
 
 function displayRandomRecipe(targetDivId) {
     const recipe = recipes[Math.floor(Math.random() * recipes.length)];
@@ -71,9 +111,7 @@ function displayRandomRecipe(targetDivId) {
 
     recipe.ingredients.forEach(imgSrc => {
         const img = document.createElement("img");
-
         const altText = imgSrc.split('/').pop().replace('.png', '');
-
         img.src = imgSrc;
         img.alt = altText;
         img.style.width = "60px";
@@ -83,34 +121,34 @@ function displayRandomRecipe(targetDivId) {
 
     container.appendChild(ingredientsDiv);
 
+    weightedPool = additionalImages.concat(
+        recipe.ingredients.flatMap(ing => Array(3).fill(ing))
+    );
+
     const zoneId = targetDivId === "Player1Recipe" ? "Player1IngredientZone" : "Player2IngredientZone";
     const animationZone = document.getElementById(zoneId);
-    animationZone.innerHTML = "";
+    animationZone.innerHTML = '';
 
     recipe.ingredients.forEach((src, index) => {
         const animatedImg = document.createElement("img");
         const altText = src.split('/').pop().replace('.png', '');
-
         animatedImg.src = src;
         animatedImg.alt = altText;
         animatedImg.classList.add("ingredient-img");
         animatedImg.draggable = true;
         animatedImg.style.animationDelay = `${index * 1}s`;
-
         animationZone.appendChild(animatedImg);
         registerInitialZone(animatedImg, animationZone);
     });
 
-    const randomImage = additionalImages[Math.floor(Math.random() * additionalImages.length)];
+    const randomImage = weightedPool[Math.floor(Math.random() * weightedPool.length)];
     const randomImg = document.createElement("img");
-    const altText = randomImage.split('/').pop().replace('.png', '');
-
+    const randomAltText = randomImage.split('/').pop().replace('.png', '');
     randomImg.src = randomImage;
-    randomImg.alt = altText;
+    randomImg.alt = randomAltText;
     randomImg.classList.add("ingredient-img");
     randomImg.draggable = true;
     randomImg.style.animationDelay = `${recipe.ingredients.length}s`;
-
     animationZone.appendChild(randomImg);
     registerInitialZone(randomImg, animationZone);
 }
@@ -118,23 +156,53 @@ function displayRandomRecipe(targetDivId) {
 const drake = dragula([document.querySelector('#Player1IngredientZone'), document.querySelector('#Player2IngredientZone')]);
 
 function registerInitialZone(imgElement, zone) {
-
     drake.on('drag', (el) => {
         el.classList.add('dragging');
     });
-
     drake.on('dragend', (el) => {
         el.classList.remove('dragging');
     });
 }
+let isIngredientInZone = {
+    Player1: false,
+    Player2: false
+};
+
+function initializeDropZones() {
+    const dropZones = [
+        document.getElementById('Player1DropZone'),
+        document.getElementById('Player2DropZone')
+    ];
+
+    drake.on('drop', (el, target) => {
+        console.log("Élément déposé", el);
+        if (target.classList.contains('drop-zone')) {
+            const player = target.id === 'Player1DropZone' ? 'Player1' : 'Player2';
+
+            if (target.children.length < maxIngredients) {
+                el.draggable = false;
+                target.appendChild(el);
+                el.style.animation = 'none';
+                el.style.position = "relative";
+                console.log(`${target.id} a maintenant ${target.children.length} ingrédients.`);
+            } else {
+                console.log("Zone déjà pleine.");
+            }
+        }
+    });
+}
+
 socket.on("GameCanBigin", () => {
     console.log("🎮 Le jeu peut commencer!");
     document.getElementById("title").style.display = "none";
     if (playerRole === "P1") {
         document.getElementById("Player1Game").style.display = "block";
         displayRandomRecipe("Player1Recipe");
+        startGame();
     } else if (playerRole === "P2") {
         document.getElementById("Player2Game").style.display = "block";
         displayRandomRecipe("Player2Recipe");
+        startGame();
     }
+    initializeDropZones();
 });
