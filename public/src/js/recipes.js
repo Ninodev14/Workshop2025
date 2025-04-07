9284c498df1da5f964b3a21f113b93b81fcdf014
JuliieRdf <julie.rondof@mmibordeaux.com>
2025-04-07 10:36:34 +0200

    add real recipe.js

--- public/src/js/recipes.js
+++ public/src/js/recipes.js
@@ -1,41 +1,112 @@
 const recipes = [{
-        name: "Pizza",
-        ingredients: [
-            "src/img/ingredients/tomato.png",
-            "src/img/ingredients/cheese.png",
-            "src/img/ingredients/dough.png",
-            "src/img/ingredients/pepperoni.png",
-            "src/img/ingredients/basil.png"
-        ]
-    },
-    {
-        name: "Salade",
-        ingredients: [
-            "src/img/ingredients/lettuce.png",
-            "src/img/ingredients/tomato.png",
-            "src/img/ingredients/cucumber.png",
-            "src/img/ingredients/olive_oil.png",
-            "src/img/ingredients/salt.png"
-        ]
-    },
-    {
         name: "Burger",
         ingredients: [
-            "src/img/ingredients/bun.png",
-            "src/img/ingredients/beef.png",
-            "src/img/ingredients/cheese.png",
-            "src/img/ingredients/lettuce.png",
-            "src/img/ingredients/tomato.png"
+            "src/img/ingredients/Steak.png",
+            "src/img/ingredients/Salade.png",
+            "src/img/ingredients/Cornichon.png",
+            "src/img/ingredients/Pain.png",
+            "src/img/ingredients/Fromage.png"
         ]
     },
     {
-        name: "Sushi",
+        name: "Lasagne",
         ingredients: [
-            "src/img/ingredients/rice.png",
-            "src/img/ingredients/seaweed.png",
-            "src/img/ingredients/salmon.png",
-            "src/img/ingredients/wasabi.png",
-            "src/img/ingredients/soy_sauce.png"
+            "src/img/ingredients/Steak.png",
+            "src/img/ingredients/Sauce tomate.png",
+            "src/img/ingredients/Pâtes.png",
+            "src/img/ingredients/Crème.png",
+            "src/img/ingredients/Fromage.png"
+        ]
+    },
+    {
+        name: "Kebab",
+        ingredients: [
+            "src/img/ingredients/Salade.png",
+            "src/img/ingredients/Poulet.png",
+            "src/img/ingredients/Tomate.png",
+            "src/img/ingredients/Sauce.png",
+            "src/img/ingredients/Pain.png"
+        ]
+    },
+    {
+        name: "Tacos",
+        ingredients: [
+            "src/img/ingredients/Pain.png",
+            "src/img/ingredients/Steack.png",
+            "src/img/ingredients/Frite.png",
+            "src/img/ingredients/Sauce.png",
+            "src/img/ingredients/Salade.png"
+        ]
+    },
+    {
+        name: "Riz cantonais",
+        ingredients: [
+            "src/img/ingredients/Riz.png",
+            "src/img/ingredients/Jambon.png",
+            "src/img/ingredients/Oeuf.png",
+            "src/img/ingredients/Petit pois.png",
+            "src/img/ingredients/Sel.png"
+        ]
+    },
+    {
+        name: "Gateau au yaourt",
+        ingredients: [
+            "src/img/ingredients/Farine.png",
+            "src/img/ingredients/Yaourt.png",
+            "src/img/ingredients/Sucre.png",
+            "src/img/ingredients/Beurre.png",
+            "src/img/ingredients/Oeuf.png"
+        ]
+    },
+    {
+        name: "Salade de fruit",
+        ingredients: [
+            "src/img/ingredients/Pomme.png",
+            "src/img/ingredients/Orange.png",
+            "src/img/ingredients/Fraise.png",
+            "src/img/ingredients/Kiwi.png",
+            "src/img/ingredients/Raisin.png"
+        ]
+    },
+    {
+        name: "Fondant au chocolat",
+        ingredients: [
+            "src/img/ingredients/Chocolat.png",
+            "src/img/ingredients/Farine.png",
+            "src/img/ingredients/Sucre.png",
+            "src/img/ingredients/Beurre.png",
+            "src/img/ingredients/Oeuf.png"
+        ]
+    },
+    {
+        name: "Tiramisu",
+        ingredients: [
+            "src/img/ingredients/Poudre de chocolat.png",
+            "src/img/ingredients/Boudoir.png",
+            "src/img/ingredients/Crème.png",
+            "src/img/ingredients/Sucre.png",
+            "src/img/ingredients/Oeuf.png"
+        ]
+    },
+    {
+        name: "Pomme au four",
+        ingredients: [
+            "src/img/ingredients/Sucre.png",
+            "src/img/ingredients/Pomme.png",
+            "src/img/ingredients/Miel.png",
+            "src/img/ingredients/Chocolat.png",
+            "src/img/ingredients/Vanille.png"
+        ]
+    },
+    {
+        name: "Tarte au pomme",
+        ingredients: [
+            "src/img/ingredients/Pomme.png",
+            "src/img/ingredients/Farine.png",
+            "src/img/ingredients/Sucre.png",
+            "src/img/ingredients/Beurre.png",
+            "src/img/ingredients/Oeuf.png"
         ]
     }
+
 ];
